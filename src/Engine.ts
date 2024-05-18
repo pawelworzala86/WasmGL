39afdc94d27a226511d6f9a0f603d32d1c283f5d
pawelworzala86 <pawelworzala86@gmail.com>
2024-05-18 16:07:00 +0200

    refactor

--- src/Engine.ts
+++ src/Engine.ts
@@ -10,7 +10,11 @@ import {
   } from './WebGL';
 
 import { getQuad } from './quad';
-import { getQuad as getD } from './worzala';
+//import { getQuad as getD } from './worzala';
+
+import { Shader } from './Shader'
+
+
 
 export declare function test(): void;
 
@@ -88,66 +92,5 @@ export class Mesh{
         }
 
         gl.drawArrays(gl.TRIANGLES, 0, this.data.length / 8);
-    }
-}
-
-const VERTEX_SHADER_CODE: string = `#version 300 es
-precision highp float;
-
-in vec3 position;
-in vec3 normal;
-in vec2 tex_coord;
-
-mat4 model = mat4(1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0);
-
-uniform mat4 projection;
-uniform mat4 camera;
-
-out vec2 tc;
-
-void main() {
-  gl_Position = projection*camera*model*vec4(position,1.0);
-  tc = tex_coord;
-}
-`;
-// THIS IS THE FRAGMENT SHADER
-const FRAGMENT_SHADER_CODE: string = `#version 300 es
-precision highp float;
-
-in vec2 tc;
-
-uniform sampler2D sampler;
-
-out vec4 color;
-
-void main() {
-  color = texture( sampler, tc );
-}
-`;
-
-export class Shader{
-    public gl: WebGLRenderingContext
-    public vertex_shader: WebGLShader
-    public fragment_shader: WebGLShader
-    public program: WebGLProgram
-    constructor(gl: WebGLRenderingContext){
-        this.gl = gl
-
-        this.vertex_shader = gl.createShader(gl.VERTEX_SHADER);
-        gl.shaderSource(this.vertex_shader, VERTEX_SHADER_CODE);
-        gl.compileShader(this.vertex_shader);
-
-        this.fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
-        gl.shaderSource(this.fragment_shader, FRAGMENT_SHADER_CODE);
-        gl.compileShader(this.fragment_shader);
-
-        this.program = gl.createProgram();
-
-        gl.attachShader(this.program, this.vertex_shader);
-        gl.attachShader(this.program, this.fragment_shader);
-
-        gl.linkProgram(this.program);
-
-        gl.useProgram(this.program);
     }
 }
--- src/madengine.ts
+++ src/madengine.ts
@@ -9,64 +9,14 @@ import {
   WebGLTexture,
 } from './WebGL';
 
-import { Mesh, Shader } from './Engine';
+import { Shader } from './Shader'
+import { Mesh } from './Engine';
 
-//testA()
 
-/*const VERTEX_SHADER_CODE: string = `#version 300 es
-  precision highp float;
-
-  in vec3 position;
-  in vec2 tex_coord;
-
-mat4 model = mat4(1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0);
-
-  uniform mat4 projection;
-  uniform mat4 camera;
-
-  out vec2 tc;
-
-  void main() {
-    gl_Position = projection*camera*model*vec4(position,1.0);
-    tc = tex_coord;
-  }
-`;
-// THIS IS THE FRAGMENT SHADER
-const FRAGMENT_SHADER_CODE: string = `#version 300 es
-  precision highp float;
-
-  in vec2 tc;
-
-  uniform sampler2D sampler;
-
-  out vec4 color;
-
-  void main() {
-    color = texture( sampler, tc );
-  }
-`;*/
-
-// initialize webgl
 var gl: WebGLRenderingContext = new WebGLRenderingContext('cnvs', 'webgl2');
 
-//  ImageData, createImage, imageReady,
-//var image_id: ImageData = gl.createImage('kaijunicorn.png');
-//var image_ready: bool = false;
-/*
-let vertex_shader: WebGLShader = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vertex_shader, VERTEX_SHADER_CODE);
-gl.compileShader(vertex_shader);
 
-let fragment_shader: WebGLShader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fragment_shader, FRAGMENT_SHADER_CODE);
-gl.compileShader(fragment_shader);
 
-let program: WebGLProgram = gl.createProgram();
-
-gl.attachShader(program, vertex_shader);
-gl.attachShader(program, fragment_shader);
-
-gl.linkProgram(program);*/
 
 let shader = new Shader(gl)
 
@@ -74,67 +24,28 @@ gl.useProgram(shader.program)
 
 
 
-/*let position_al: GLint = gl.getAttribLocation(program, 'position');
-gl.enableVertexAttribArray(position_al);
-
-let tex_coord_al: GLint = gl.getAttribLocation(program, 'tex_coord');
-gl.enableVertexAttribArray(tex_coord_al);*/
-
 gl.enable(gl.BLEND);
 gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
 
 let mesh:Mesh = new Mesh(gl, shader);
-//let quad_data: StaticArray<f32> = getQuad()
 
 
 
-//let texture: WebGLTexture = gl.createTexture();
-//let sampler: WebGLUniformLocation = gl.getUniformLocation(program, 'sampler');
-//let projectionSampler: WebGLUniformLocation = gl.getUniformLocation(program, 'projection');
+
+
 
 let projection_matrix: StaticArray<f32> = [1.3737387097273113,0.0,0.0,0.0,0.0,1.3737387097273113,0.0,0.0,0.0,0.0,-1.02020202020202,-1.0,0.0,0.0,-2.0202020202020203,0.0]
 let camera_matrix: StaticArray<f32> = [1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,-10.100090086,1.0]
 
-//let buffer: WebGLBuffer = gl.createBuffer();
-//gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-//gl.bufferData<f32>(gl.ARRAY_BUFFER, quad_data, gl.STATIC_DRAW);
-//let bufferReady = false
+
+
+
 
 export function displayLoop(): void {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  /*if (image_ready == false) {
-    if (gl.imageReady(image_id) == false) {
-      return;
-    }
-
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
-    gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, +true);
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image_id);
-
-    gl.uniform1i(sampler, 0);
-    image_ready = true;
-  }*/
+ 
   mesh.render(projection_matrix, camera_matrix)
-
-
-  
-  /*gl.uniformMatrix4fv(projectionSampler, false, projection_matrix)
-
-  if(!bufferReady){
-    gl.bufferData<f32>(gl.ARRAY_BUFFER, mesh.data, gl.STATIC_DRAW);
-    bufferReady=true
-  }
-
-  //vertexAttribPointer     attribute |  dimensions | data type | normalize | stride bytes | offset bytes
-  gl.vertexAttribPointer(position_al, 3, gl.FLOAT, +false, 20, 0);
-  gl.vertexAttribPointer(tex_coord_al, 2, gl.FLOAT, +false, 20, 12);
-
-  gl.drawArrays(gl.TRIANGLES, 0, mesh.data.length / 5);*/
 }
